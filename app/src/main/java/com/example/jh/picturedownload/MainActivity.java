d74a961bf7807687b69fb1ca5dbdecf0ba3fe139
jinhuizxc <1004260403@qq.com>
2018-04-16 17:40:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/picturedownload/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jh.picturedownload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 此前没有整理过mvp图片下载，保存本地的demo，
+ *
+ * 这次写下，整合资源，后续会整合到androiddemo中
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
